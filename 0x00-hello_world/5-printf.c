5c9c824149654e898617318d75972a924cb4226a
auleki <kodagiwa@gmail.com>
2022-12-12 01:42:25 +0100

    complete task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+* main - this is a function that prints a certain string with a new line
+*
+* Return: returns 0 and ends the program
+*/
 int main (void)
 {
     printf('with proper grammar, but the outcome is a piece of art \n');
-    return 0;
+    return (0);
 }
