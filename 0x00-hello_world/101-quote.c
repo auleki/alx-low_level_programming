3406413be6b3ff203b4bab8ce49962d35e0ad0c0
auleki <kodagiwa@gmail.com>
2022-12-12 02:04:05 +0100

    complete advance task 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unstd.h>
+
+/**
+* main - a function that print a phrase
+*
+* Return: Returns 1 this time, ends program still
+*/
+
+void main()
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
