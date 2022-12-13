f7b96d0daa222e1eca49ef764fffcde6ebdd2b27
auleki <kodagiwa@gmail.com>
2022-12-13 05:41:41 +0100

    complete task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-int main (void) 
+/**
+* main - logs an alphabet in lowercase followed by the uppercase equivalent
+*
+* Return: returns 0 and exits the program
+*/
+int main(void)
 {
-  char ch;
+char ch;
 
-  for (ch = 'a'; ch <= 'z'; ch++) {
-    putchar(ch);
-  }
-   
-  for (ch = 'A'; ch <= 'Z'; ch++) {
-    putchar(ch);
-  }
-  
-  return 0;
+for (ch = 'a'; ch <= 'z'; ch++) {
+putchar(ch);
+}
+
+for (ch = 'A'; ch <= 'Z'; ch++) {
+putchar(ch);
+}
+putchar('\n');
+
+return 0;
 }
