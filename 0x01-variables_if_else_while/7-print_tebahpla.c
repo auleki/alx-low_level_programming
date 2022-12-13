42637231a0ccd2f1b2e0888cb0a817215a84d0b5
auleki <kodagiwa@gmail.com>
2022-12-13 06:00:12 +0100

    complete task 6

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-int main (void) 
+/**
+* main - logs an alphabet in lowercase
+*
+* Return: returns 0 and exits the program
+*/
+int main(void)
 {
-  char ch;
-  for (ch = 'z'; ch >= 'a'; ch--) {
-    putchar(ch);
-  }
-  return 0;
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+return (0);
 }
