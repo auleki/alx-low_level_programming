427a6a9aefc95a12dddf9eccf1e285b64a222c61
auleki <kodagiwa@gmail.com>
2022-12-13 05:49:15 +0100

    complete task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-int main (void) 
+/**
+* main - logs an alphabet in lowercase
+*
+* Return: returns 0 and exits the program
+*/
+int main(void)
 {
-	int limit;
+int limit;
 
-	limit = 10;
+limit = 10;
 
-	for (int i = 0; i < limit; i++)
-	{
-		printf("%d", i);
-	}	
-	
-	putchar(10);
-	
-	return 0;
+for (int i = 0; i < limit; i++)
+{
+printf("%d", i);
+}
+putchar(10);
+putchar('\n');
+return (0);
 }
