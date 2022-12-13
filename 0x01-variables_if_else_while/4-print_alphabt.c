6a5a87f5126f8724e413229e6d7a91e17fbcbc3f
auleki <kodagiwa@gmail.com>
2022-12-13 05:46:25 +0100

    complete task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-
-int main (void) 
+/**
+* main - logs an alphabet in lowercase followed by a new line
+*
+* Return: returns 0 and exits the program
+*/
+int main(void) 
 {
-  char ch;
+char ch;
 
-  for (ch = 'a'; ch <= 'z'; ch++) {
-    if ((ch != 'e') && (ch != 'q'))
-    {
-      putchar(ch);
-    }
-  }
-  
-  return 0;
+for (ch = 'a'; ch <= 'z'; ch++) 
+{
+if ((ch != 'e') && (ch != 'q'))
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
 }
