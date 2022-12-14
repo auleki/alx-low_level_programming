948250db7b73b548b63a85c36d0a71ecee324a96
auleki <kodagiwa@gmail.com>
2022-12-14 05:23:02 +0100

    complete task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,11 @@
 #include "main.h"
-
+/**
+* print_alphabet - prints a through z of in lowercase
+*/
 void print_alphabet(void)
 {
 char a = 'a';
-while (a < 'z')
+while (a <= 'z')
 {
 _putchar(a);
 a++;
