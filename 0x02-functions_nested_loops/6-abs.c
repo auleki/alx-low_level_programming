4c98000dbbf6f1bcea796ba415e4938e04027b44
auleki <kodagiwa@gmail.com>
2022-12-14 22:39:05 +0100

    complete task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _abs - gives the absolute value of a value
+* @n: a number
+*
+* Description: a value is given and and absolute
+* value is returned 
+*
+* Return: the absolute value of a value
+*/
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n*-1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (n);
+}
+}
