c0f3cd1b3a48beadcd84823c2ae360172f700947
auleki <kodagiwa@gmail.com>
2022-12-14 04:17:33 +0100

    complete task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/** 
+* main - entry point of the program where a certain string is printed
+* 
+* Return: returns 0 and stops the program
+* /
+
+int main(void)
+{
+printf("_putchar");
+printf("\n");
+}
