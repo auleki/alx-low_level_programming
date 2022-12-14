b67b105089452ab4f0310f62789307594ca86456
auleki <kodagiwa@gmail.com>
2022-12-14 22:02:45 +0100

    complete task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,9 @@
 /**
 * _islower - Checks if character is lowercase
 * @c: character
-* 
-* Description: Checks if a character is lowercase and returns either true or false
+*
+* Description: Checks if a character is lowercase 
+* and returns either true or false
 *
 * Return: either 1 or 0 which mean either true or false
 */
