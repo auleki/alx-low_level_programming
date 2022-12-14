dee03a18b851e6e96dd8eb3c80b3204aedcbfd78
auleki <kodagiwa@gmail.com>
2022-12-14 05:30:05 +0100

    complete task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints a through z in lowercase ten times over
+*/
+void print_alphabet_x10(void)
+{
+char a = 'a';
+for (int i = 0; i < 11; i++)
+{
+while (a <= 'z)
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+}
+}
