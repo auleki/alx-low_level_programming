6de9e2342758e89cff2a71d2ac914dd867a2dffc
auleki <kodagiwa@gmail.com>
2022-12-23 00:34:19 +0100

    adjust spacing within file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,27 +4,29 @@
 * _strncat - concatenates two strings
 * @dest: string to append to
 * @src: string to append
-
+*
 * Return: a pointer to the new concatenated string
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+int i, j;
 
-	i = 0;
-	j = 0;
+i = 0;
+j = 0;
 
-	while (dest[i] != '\0')
-		i++;
+while (dest[i] != '\0')
+{
+i++;
+}
 
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
 
-	dest[i] = '\0';
+dest[i] = '\0';
 
-	return (dest);
+return (dest);
 }
