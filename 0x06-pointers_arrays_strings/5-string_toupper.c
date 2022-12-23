007f2c0f7da87a1bf751d17223103588e96f6c5d
auleki <kodagiwa@gmail.com>
2022-12-23 01:33:37 +0100

    complete task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,17 +5,18 @@
  *
  * Return: the pointer to dest
  */
-
 char *string_toupper(char *s)
 {
-	int i = 0;
+int i = 0;
 
-	while (*(s + i) != '\0')
-	{
-		if ((*(s + i) >= 97) && (*(s + i) <= 122))
-			*(s + i) = *(s + i) - 32;
-		i++;
-	}
+while (*(s + i) != '\0')
+{
+if ((*(s + i) >= 97) && (*(s + i) <= 122))
+{
+*(s + i) = *(s + i) - 32;
+}
+i++;
+}
 
-	return (s);
+return (s);
 }
