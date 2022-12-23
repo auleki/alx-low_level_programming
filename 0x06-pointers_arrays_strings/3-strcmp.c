45431ce95a58f5a1f5f1357bcb48081ee1585170
auleki <kodagiwa@gmail.com>
2022-12-23 01:36:05 +0100

    complete task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _strcmp - checks if two strings are equal
- * @s1: first string
- * @s2: second string
-
- * Return: 0 if s1 and s2 are equals, return another number if not
- */
+* _strcmp - checks if two strings are equal
+* @s1: first string
+* @s2: second string
+*
+* Return: 0 if s1 and s2 are equals, return another number if not
+*/
 
 int _strcmp(char *s1, char *s2)
 {
