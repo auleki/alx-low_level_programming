c167180d470a83ee5aa59c922d6ff017136242bf
auleki <kodagiwa@gmail.com>
2023-01-04 04:33:38 +0100

    complete task 3 & 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* _strpbrk - searches a string for any of a set of bytes.
+*
+* @s: string to search through
+* @accept: substring to search for
+* Return: Returns the number of bytes in the initial segment
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+
+i = 0;
+while (s[i] != '\0')
+{
+j = 0;
+while (accept[j] != '\0')
+{
+if (s[i] == s[j])
+{
+return (s + i);
+}
+j++;
+}
+i++;
+}
+return (NULL);
+}
