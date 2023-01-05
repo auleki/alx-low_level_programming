6575d0d603eb9cad9603a46e202352865a70aafc
auleki <kodagiwa@gmail.com>
2023-01-05 01:40:04 +0100

    complete task 1

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 
 void _puts_recursion(char *s);
 int factorial(int n);
 int _putchar(char c);
+void _print_rev_recursion(char *s);
+#endif // MAIN_H
